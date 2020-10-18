996359567a91a3e7dd0d1311258dea41a584b56f
gneris177 <gneris177@gmail.com>
2020-10-18 00:44:15 -0300

    Novo Design

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,19 @@
-const menu = document.querySelector('.mini-menu')
-const iconMenu = document.querySelector('.icon-menu')
-const titulo = document.querySelector('.titulo-1')
+const abrirMenu = document.querySelector('.icon-menu')
+const fecharMenu = document.querySelector('.icon-close')
+const menu = document.querySelector('.cabecalho-itens-mobile')
 
+abrirMenu.onclick = () => menuMobile()
 
-iconMenu.onclick = () => {
+const menuMobile = () => {
     menu.style.display = 'flex'
-    iconMenu.style.display = 'none'
-    titulo.style.display = 'none'   
+    abrirMenu.style.display = 'none'
+    fecharMenu.style.display = 'block'
+}
+
+fecharMenu.onclick = () => fecha()
+
+const fecha = () => {
+    menu.style.display = 'none'
+    abrirMenu.style.display = 'block'
+    fecharMenu.style.display = 'none'
 }
